ebef2e7b0764ed4471cabd62277388c4f872fd0c
Manthan Kasle <manthankaslemk@gmail.com>
2024-03-25 01:49:39 +0530

    Added Login Page

--- edutechforge/src/App.jsx
+++ edutechforge/src/App.jsx
@@ -1,22 +1,20 @@
 import './App.css'
-import {Button} from "../src/components/ui/button"
-import Navbar from './components/ui/navabar'
-import HeroSection from './sections/herosection'
-import Footer from './sections/footer'
-import Error from './components/ui/errorpage'
+
+import Login from './sections/login'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './sections/home'
 
 function App() {
-  
-
   return (
-    <>
-    <Navbar/>
-   <Button>Hello EveryOne</Button> 
-   <HeroSection/>
-   <Error/>
-   <Footer/>
-     
-    </>
+
+    <div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+    </BrowserRouter>
+    </div>
   )
 }
 
---
+++ edutechforge/src/sections/home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '@/components/ui/navabar'
+import HeroSection from './herosection'
+import Error from '@/components/ui/errorpage'
+import Footer from './footer'
+import { Button } from '@/components/ui/button'
+
+const home = () => {
+  return (
+    <div>
+          <Navbar />
+        <Button>Hello Everyone</Button>
+        <HeroSection />
+        <Error />
+        <Footer />
+        
+    </div>
+  )
+}
+
+export default home
