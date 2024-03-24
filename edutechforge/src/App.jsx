d0aeb4787d5435375a02701239126cba741e6248
Manthan Kasle <manthankaslemk@gmail.com>
2024-03-25 01:06:06 +0530

    Added Error Page

--- edutechforge/src/App.jsx
+++ edutechforge/src/App.jsx
@@ -1,11 +1,20 @@
-
 import './App.css'
+import {Button} from "../src/components/ui/button"
+import Navbar from './components/ui/navabar'
+import HeroSection from './sections/herosection'
+import Footer from './sections/footer'
+import Error from './components/ui/errorpage'
 
 function App() {
   
 
   return (
     <>
+    <Navbar/>
+   <Button>Hello EveryOne</Button> 
+   <HeroSection/>
+   <Error/>
+   <Footer/>
      
     </>
   )
