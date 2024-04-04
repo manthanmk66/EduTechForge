0ca933267a8ec1bb8375767f4af0c5aecb2ecf95
Manthan Kasle <manthankaslemk@gmail.com>
2024-04-04 23:50:20 +0530

    Added Quotes and headings

--- edutechforge/src/common/core/AboutPage/LearningGrid.jsx
+++ edutechforge/src/common/core/AboutPage/LearningGrid.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import HighlightText from "../../../components/core/HomePage/HighlightText";
-import CTAButton from "../../../components/core/HomePage/Button";
+import HighlightText from "../../../sections/Home/Highlighttext";
+import { Button } from "../../../components/ui/button";
 
 const LearningGridArray = [
   {
     order: -1,
     heading: "World-Class Learning for",
-    highliteText: "Anyone, Anywhere",
+    highlightText: "Anyone, Anywhere",
     description:
       "Studynotion partners with more than 275+ leading universities and companies to bring flexible, affordable, job-relevant online learning to individuals and organizations worldwide.",
     BtnText: "Learn More",
@@ -46,16 +46,16 @@ const LearningGridArray = [
 
 const LearningGrid = () => {
   return (
-    <div className="grid mx-auto w-[350px] xl:w-fit grid-cols-1 xl:grid-cols-4 mb-12">
+    <div className="grid mx-auto w-[350px] pl-4 pt-4  text-white xl:w-fit grid-cols-1 xl:grid-cols-4 mb-12">
       {LearningGridArray.map((card, i) => {
         return (
           <div
             key={i}
             className={`${i === 0 && "xl:col-span-2 xl:h-[294px]"}  ${
               card.order % 2 === 1
-                ? "bg-richblack-700 h-[294px]"
+                ? "bg-neutral-600 h-[294px]"
                 : card.order % 2 === 0
-                ? "bg-richblack-800 h-[294px]"
+                ? "bg-slate-800 h-[294px]"
                 : "bg-transparent"
             } ${card.order === 3 && "xl:col-start-2"}  `}
           >
@@ -65,23 +65,19 @@ const LearningGrid = () => {
                   {card.heading}
                   <HighlightText text={card.highliteText} />
                 </div>
-                <p className="text-richblack-300 font-medium">
-                  {card.description}
-                </p>
+                <p className="text-white font-medium">{card.description}</p>
 
                 <div className="w-fit mt-2">
-                  <CTAButton active={true} linkto={card.BtnLink}>
+                  <Button active={true} linkto={card.BtnLink}>
                     {card.BtnText}
-                  </CTAButton>
+                  </Button>
                 </div>
               </div>
             ) : (
               <div className="p-8 flex flex-col gap-8">
-                <h1 className="text-richblack-5 text-lg">{card.heading}</h1>
+                <h1 className="text-white text-lg">{card.heading}</h1>
 
-                <p className="text-richblack-300 font-medium">
-                  {card.description}
-                </p>
+                <p className="text-slate-400 font-medium">{card.description}</p>
               </div>
             )}
           </div>
--- edutechforge/src/common/core/AboutPage/Quote.jsx
+++ edutechforge/src/common/core/AboutPage/Quote.jsx
@@ -3,7 +3,7 @@ import HighlightText from "../../../sections/Home/Highlighttext";
 
 const Quote = () => {
   return (
-    <div className=" text-xl md:text-4xl font-semibold mx-auto py-5 pb-20 text-center text-white">
+    <div className=" text-xl md:text-4xl font-semibold mx-auto py-5 pb-20 text-center text-black">
       We are passionate about revolutionizing the way we learn. Our innovative
       platform <HighlightText text={"combines technology"} />,{" "}
       <span className="bg-gradient-to-b from-[#FF512F] to-[#F09819] text-transparent bg-clip-text font-bold">
