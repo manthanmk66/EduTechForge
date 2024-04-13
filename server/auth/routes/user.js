8a2732b09216b89089c859639336eb27a9c73143
Manthan Kasle <manthankaslemk@gmail.com>
2024-04-13 11:57:49 +0530

    Added MiddleWares

--- server/auth/routes/user.js
+++ server/auth/routes/user.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const { login, signup } = require("../Controllers/auth");
+const {auth,isStudent,isAdmin}= require("../middlewares/auth");
+
 
 router.post("/login",login);
 router.post("/signup", signup);
+
+//protected Routes
 
 module.exports = router;
